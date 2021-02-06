ba21de655e127ad1ef2001b4dd6995446629fae2
amchavan <amchavan>
2021-02-06 13:35:11 +0100

    Renamed project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-training0'`, () => {
+  it(`should have as title 'angular-training'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-training0');
+    expect(app.title).toEqual('angular-training');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular-training0 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('angular-training app is running!');
   });
 });
