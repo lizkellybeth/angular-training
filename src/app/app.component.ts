0f89ee3e9523526987190af2da58f180813d17d7
amchavan <amchavan>
2021-02-08 11:04:12 +0100

    Added solution to exercise 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BibliographyService} from './bibliography.service';
 import { BibliographicRecord } from './bibliographic-record';
+import {ProposalsService} from './proposals.service';
+import {Proposal} from './proposal';
 
 @Component({
     selector: 'app-root',
@@ -10,17 +12,30 @@ import { BibliographicRecord } from './bibliographic-record';
 export class AppComponent implements OnInit {
 
     bibliography: string;
+    proposalTable: string;
 
-    constructor( private bibliographyService: BibliographyService ) {
+    constructor( private bibliographyService: BibliographyService,
+                 private proposalService: ProposalsService ) {
     }
 
     ngOnInit(): void {
         this.bibliographyService.fetchBibliography( 3, this.getBibliographyCallback() );
+        this.proposalService.fetchBProposals( '2018.A', this.getProposalsCallback() );
     }
 
     private getBibliographyCallback(): (bibliography: BibliographicRecord[]) => void {
         return (bibliography) => {
             this.bibliography = JSON.stringify(bibliography, undefined, 4);
+        };
+    }
+
+    private getProposalsCallback(): (proposals: Proposal[]) => void {
+        return (proposals) => {
+            let proposalsTable = '';
+            proposals.forEach(proposal => {
+                proposalsTable += `${proposal.code.padEnd(15)} ${proposal.piName.padEnd(30)} ${proposal.title}\n`;
+            });
+            this.proposalTable = proposalsTable;
         };
     }
 }
---
+++ src/app/proposal.ts
@@ -0,0 +1,5 @@
+export interface Proposal {
+    code: string;
+    piName: string;
+    title: number;
+}
