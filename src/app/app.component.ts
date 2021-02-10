5bac90dd2a792b5450d88acc4777c5922bf525a0
amchavan <amchavan>
2021-02-10 13:28:58 +0100

    Unit 1, follow-up: completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,16 +16,25 @@ export class AppComponent implements OnInit {
 
     ngOnInit(): void {
 
+        // Solution 1: handle with success and failure in the client code
         this.gitHubOrganizationsService
-            .fetchOrganizations( 3 )
+            .fetchOrganizations1( 3 )
+            .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ))
+            .catch( error => console.error( '>>> Local error handling', JSON.stringify( error )));
+
+        // Solution 2a: centralized error handling, inline callback
+        this.gitHubOrganizationsService
+            .fetchOrganizations2( 3 )
             .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ));
 
+        // Solution 2b: centralized error handling, fully-typed callback
         this.gitHubOrganizationsService
-            .fetchOrganizations( 3 )
+            .fetchOrganizations2( 3 )
             .then( this.organizationsCallback );
 
+        // Solution 3: central error handling is disabled
         this.gitHubOrganizationsService
-            .fetchOrganizations( 3, false )
+            .fetchOrganizations3( 3, false )
             .then( this.organizationsCallback )
             .catch( error => console.error( '>>> Local error handling', JSON.stringify( error )));
     }
--- src/app/git-hub-organizations.service.ts
+++ src/app/git-hub-organizations.service.ts
@@ -8,7 +8,8 @@ import {environment} from '../environments/environment.prod';
 })
 export class GitHubOrganizationsService {
 
-    private organizationsUrl = environment.gitHubApiUrl + '/organizations-not-found?per_page=';
+    // This URL will always cause a '404 Not Found' error
+    private organizations404Url = environment.gitHubApiUrl + '/organizations/not-found?per_page=';
 
     static errorHandler( error: any ): void {
         console.error( '>>> Central error handling:', JSON.stringify( error ));
@@ -17,16 +18,22 @@ export class GitHubOrganizationsService {
     constructor( private httpClient: HttpClient ) {
     }
 
-    /**
-     * Perform an HTTP request to the 'organizations' endpoint of the GitHub API.
-     *
-     * @return  A Promise containing an array of GitHubOrganization instances
-     *          if all went well; or 'undefined' if the HTTP request failed
-     *          with an error.
-     */
-    fetchOrganizations( count: number,
+    fetchOrganizations1(count: number ): Promise< void | GitHubOrganization[] > {
+        return this.httpClient
+            .get<GitHubOrganization[]>( this.organizations404Url + count )
+            .toPromise();
+    }
+
+    fetchOrganizations2(count: number ): Promise< void | GitHubOrganization[] > {
+        return this.httpClient
+            .get<GitHubOrganization[]>( this.organizations404Url + count )
+            .toPromise()
+            .catch( GitHubOrganizationsService.errorHandler );
+    }
+
+    fetchOrganizations3(count: number,
                         catchErrors: boolean = true ): Promise< void | GitHubOrganization[] > {
-        const promise = this.httpClient.get<GitHubOrganization[]>( this.organizationsUrl + count ).toPromise();
+        const promise = this.httpClient.get<GitHubOrganization[]>( this.organizations404Url + count ).toPromise();
         return catchErrors
             ? promise.catch( GitHubOrganizationsService.errorHandler )
             : promise;
