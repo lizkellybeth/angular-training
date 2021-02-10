98e7adbb2117d5278d95fbdf24107e189174d0ec
amchavan <amchavan>
2021-02-10 15:31:44 +0100

    Unit 1 follow-up: fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GitHubOrganizationsService } from './git-hub-organizations.service';
-import { GitHubOrganization } from './git-hub-organization';
 
 @Component({
     selector: 'app-root',
@@ -22,28 +21,15 @@ export class AppComponent implements OnInit {
             .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ))
             .catch( error => console.error( '>>> Local error handling', JSON.stringify( error )));
 
-        // Solution 2a: centralized error handling, inline callback
+        // Solution 2: centralized error handling
         this.gitHubOrganizationsService
             .fetchOrganizations2( 3 )
             .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ));
 
-        // Solution 2b: centralized error handling, fully-typed callback
-        this.gitHubOrganizationsService
-            .fetchOrganizations2( 3 )
-            .then( this.organizationsHandler );
-
         // Solution 3: central error handling is disabled, fully-typed callback
         this.gitHubOrganizationsService
             .fetchOrganizations3( 3, false )
-            .then( this.organizationsHandler )
+            .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ))
             .catch( error => console.error( '>>> Local error handling', JSON.stringify( error )));
-    }
-
-    private organizationsHandler( organizations: void | GitHubOrganization[] ): void {
-        // The input parameter organizations can be undefined if an error
-        // conditions was triggered
-        if ( organizations ) {
-            this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 );
-        }
     }
 }
