4e4fa3098edfbeee2641df7c6c9605984b831534
amchavan <amchavan>
2021-02-10 15:28:12 +0100

    Unit 2 start: creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { GitHubOrganizationsService } from './git-hub-organizations.service';
-import { GitHubOrganization } from './git-hub-organization';
 
 @Component({
     selector: 'app-root',
@@ -15,35 +14,8 @@ export class AppComponent implements OnInit {
     }
 
     ngOnInit(): void {
-
-        // Solution 1: handle with success and failure in the client code
         this.gitHubOrganizationsService
-            .fetchOrganizations1( 3 )
-            .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ))
-            .catch( error => console.error( '>>> Local error handling', JSON.stringify( error )));
-
-        // Solution 2a: centralized error handling, inline callback
-        this.gitHubOrganizationsService
-            .fetchOrganizations2( 3 )
-            .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ));
-
-        // Solution 2b: centralized error handling, fully-typed callback
-        this.gitHubOrganizationsService
-            .fetchOrganizations2( 3 )
-            .then( this.organizationsHandler );
-
-        // Solution 3: central error handling is disabled, fully-typed callback
-        this.gitHubOrganizationsService
-            .fetchOrganizations3( 3, false )
-            .then( this.organizationsHandler )
-            .catch( error => console.error( '>>> Local error handling', JSON.stringify( error )));
-    }
-
-    private organizationsHandler( organizations: void | GitHubOrganization[] ): void {
-        // The input parameter organizations can be undefined if an error
-        // conditions was triggered
-        if ( organizations ) {
-            this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 );
-        }
+            .fetchOrganizations( 3 )
+            .then( organizations => this.gitHubOrganizations = JSON.stringify( organizations, undefined, 4 ) );
     }
 }
--- src/app/git-hub-organizations.service.ts
+++ src/app/git-hub-organizations.service.ts
@@ -9,7 +9,7 @@ import {environment} from '../environments/environment.prod';
 export class GitHubOrganizationsService {
 
     // This URL will always cause a '404 Not Found' error
-    private organizations404Url = environment.gitHubApiUrl + '/organizations/not-found?per_page=';
+    private organizationsUrl = environment.gitHubApiUrl + '/organizations?per_page=';
 
     static errorHandler( error: any ): void {
         console.error( '>>> Central error handling:', JSON.stringify( error ));
@@ -18,22 +18,9 @@ export class GitHubOrganizationsService {
     constructor( private httpClient: HttpClient ) {
     }
 
-    fetchOrganizations1(count: number ): Promise< void | GitHubOrganization[] > {
-        return this.httpClient
-            .get<GitHubOrganization[]>( this.organizations404Url + count )
-            .toPromise();
-    }
-
-    fetchOrganizations2(count: number ): Promise< void | GitHubOrganization[] > {
-        return this.httpClient
-            .get<GitHubOrganization[]>( this.organizations404Url + count )
-            .toPromise()
-            .catch( GitHubOrganizationsService.errorHandler );
-    }
-
-    fetchOrganizations3(count: number,
+    fetchOrganizations( count: number,
                         catchErrors: boolean = true ): Promise< void | GitHubOrganization[] > {
-        const promise = this.httpClient.get<GitHubOrganization[]>( this.organizations404Url + count ).toPromise();
+        const promise = this.httpClient.get<GitHubOrganization[]>( this.organizationsUrl + count ).toPromise();
         return catchErrors
             ? promise.catch( GitHubOrganizationsService.errorHandler )
             : promise;
