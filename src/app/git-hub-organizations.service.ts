d187222a12dea2f03f4eca2a4b4855f14a52cf09
amchavan <amchavan>
2021-02-12 15:39:49 +0100

    Unit 3: Added paginating service method

--- src/app/git-hub-organizations.service.ts
+++ src/app/git-hub-organizations.service.ts
@@ -8,21 +8,89 @@ import {environment} from '../environments/environment.prod';
 })
 export class GitHubOrganizationsService {
 
-    // This URL will always cause a '404 Not Found' error
+    private readonly pageMarkers: number[];
+
     private organizationsUrl = environment.gitHubApiUrl + '/organizations?per_page=';
 
     static errorHandler( error: any ): void {
-        console.error( '>>> Central error handling:', JSON.stringify( error ));
+        const errorString = error as Error ? error.message : JSON.stringify( error );
+        console.error( '>>> Central error handling:', errorString );
     }
 
     constructor( private httpClient: HttpClient ) {
+        this.pageMarkers = [0];
     }
 
     fetchOrganizations( count: number,
                         catchErrors: boolean = true ): Promise< void | GitHubOrganization[] > {
+        // See https://docs.github.com/en/rest/reference/orgs#list-organizations
         const promise = this.httpClient.get<GitHubOrganization[]>( this.organizationsUrl + count ).toPromise();
         return catchErrors
             ? promise.catch( GitHubOrganizationsService.errorHandler )
             : promise;
+    }
+
+    fetchOrganizationsPage( pageSize: number,
+                            page = 1,
+                            catchErrors: boolean = true ): Promise< void | GitHubOrganization[] > {
+
+        const promise = this.fetchOrganizationsPageInternal( pageSize, page );
+        return catchErrors
+            ? promise.catch( error => GitHubOrganizationsService.errorHandler( error ))
+            : promise;
+    }
+
+    /** Rudimentary paging queries of the organizations API -- don't use in production :-) */
+    private fetchOrganizationsPageInternal( pageSize: number, page: number ): Promise< void | GitHubOrganization[] > {
+
+        // Check page number: it should
+        if ( page < 1 || page > this.pageMarkers.length ) {
+            const errorMessage = `Invalid page number: ${page}, should be between 1 and ${this.pageMarkers.length}`;
+            return new Promise( (resolve, reject) => reject( new RangeError( errorMessage )));
+        }
+
+        // If this is the first call, ignore the page number and get the first page
+        if ( this.pageMarkers.length === 1 ) {
+            page = 1;
+        }
+
+        // Build the organization page URL, see https://docs.github.com/en/rest/reference/orgs#list-organizations
+        // Need to retrieve entries whose ID is larger than the largest ID of the previous page
+        const previousPage = page - 1;
+        const previousPageMarker = this.pageMarkers[ previousPage ];
+        const url = this.organizationsUrl + pageSize + '&since=' + previousPageMarker;
+
+        // Read the requested page and, before we return the promise, save
+        // the ID of the last organization in the pageMarkers array
+        const promise = this.httpClient.get<GitHubOrganization[]>( url ).toPromise();
+        promise.then( (organizationPage) => {
+            const actualPageSize = organizationPage.length;
+            const lastOrganizationIndex = actualPageSize - 1;
+            const lastOrganization = organizationPage[ lastOrganizationIndex ];
+            this.pageMarkers[ page ] = lastOrganization.id;
+        });
+
+        return promise;
+    }
+
+    // Test method for fetchOrganizationsPage();
+    async fetchOrganizationsPageTest(): Promise<void> {
+        await this.fetchOrganizationsPage( 10, 0 );    // ERROR
+
+        await this.fetchOrganizationsPage( 10, 2 );    // ERROR
+
+        await this.fetchOrganizationsPage( 10, 1 )
+            .then( organizations => console.log( '10, 1', organizations ));
+
+        await this.fetchOrganizationsPage( 10, 2 )
+            .then( organizations => console.log(  '10, 2', organizations ));
+
+        await this.fetchOrganizationsPage( 10, 3 )
+            .then( organizations => console.log(  '10, 3', organizations ));
+
+        await this.fetchOrganizationsPage( 10, 2 )
+            .then( organizations => console.log(  '10, 2', organizations ));
+
+        await this.fetchOrganizationsPage( 10, 5 );    // ERROR
     }
 }
